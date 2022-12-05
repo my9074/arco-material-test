5517ce5e0307da4c6ebfad316cb360f4b727063a
miaoyu3 <miaoyu3@jd.com>
2022-12-05 17:10:27 +0800

    arco-cli: initialize arco-marterial-test-page

---
+++ .config/jest.config.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+const { getGitRootPath } = require('arco-cli-dev-utils');
+
+module.exports = {
+  /**
+   * @param config {import('arco-scripts').JestConfig}
+   */
+  node: (config) => {
+    const rootConfigPath = path.resolve(getGitRootPath(), 'arco.scripts.config.js');
+    if (fs.existsSync(rootConfigPath)) {
+      const { jest } = require(rootConfigPath);
+      if (jest && jest.node) {
+        config = jest.node(config) || config;
+      }
+    }
+
+    config.moduleNameMapper = {
+      '^arco-marterial-test-page/(.+)$': '<rootDir>/$1',
+      '^arco-marterial-test-page$': '<rootDir>',
+    };
+  },
+  /**
+   * @param config {import('arco-scripts').JestConfig}
+   */
+  client: (config) => {
+    const rootConfigPath = path.resolve(getGitRootPath(), 'arco.scripts.config.js');
+    if (fs.existsSync(rootConfigPath)) {
+      const { jest } = require(rootConfigPath);
+      if (jest && jest.client) {
+        config = jest.client(config) || config;
+      }
+    }
+
+    config.moduleNameMapper = {
+      '^arco-marterial-test-page/(.+)$': '<rootDir>/$1',
+      '^arco-marterial-test-page$': '<rootDir>',
+    };
+  },
+};
---
+++ stories/stories.jsx
@@ -0,0 +1,5 @@
+export * from '../src/demo';
+
+export default {
+  title: 'arco-marterial-test-page',
+};
